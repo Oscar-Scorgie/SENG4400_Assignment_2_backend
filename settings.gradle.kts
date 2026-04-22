c5f27dfc441f02f7efb0a3063251def3865d9ace
oscar <oscar.scorgie@gmail.com>
2026-04-22 05:57:57 +0000

    yaml changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 rootProject.name = "demo"
 
 include("login", "transactions", "analyticsDashboard")
+
+project(":login").projectDir = file("login/demo")
+project(":transactions").projectDir = file("transactions/demo")
+project(":analyticsDashboard").projectDir = file("analyticsDashboard/demo")
